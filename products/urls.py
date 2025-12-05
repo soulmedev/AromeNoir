76cbfd69f088f0694d7ac3ed9494a79a1d6c6d1e
soulmedev <lysak2233@gmail.com>
2025-12-05 20:47:35 +0100

    upd

--- products/urls.py
+++ products/urls.py
@@ -6,6 +6,7 @@ app_name = 'products'
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('collection/', views.collection, name='collection'),
     path('products/', views.product_list, name='product_list'),
     path('products/<slug:category_slug>/', views.product_list, name='product_list_by_category'),
     path('product/<slug:slug>/', views.product_detail, name='product_detail'),
